bf97ca201369e3da60ba0c88f3555fba39f3821c
claudemiro <dimiro1@gmail.com>
2016-03-21 20:41:45 -0300

    Addind params to context

--- net-context/main.go
+++ net-context/main.go
@@ -17,15 +17,22 @@ func (h appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	h.Handler(h.ctx, w, r)
 }
 
+func param(ctx context.Context, name string) string {
+	return ctx.Value(name).(string)
+}
+
 func main() {
-	ctx := context.WithValue(context.Background(), "Name", "Application Name")
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "Name", "Application Name")
+	ctx = context.WithValue(ctx, "Version", "1.0.0")
 
 	http.Handle("/", appHandler{ctx: ctx, Handler: indexHandler})
 	http.ListenAndServe(":8080", nil)
 }
 
 func indexHandler(ctx context.Context, w http.ResponseWriter, r *http.Request) {
-	name := ctx.Value("Name").(string)
+	name := param(ctx, "Name")
+	version := param(ctx, "Version")
 
-	fmt.Fprintf(w, "indexHandler: name is %s", name)
+	fmt.Fprintf(w, "indexHandler: name is %s and version is %s", name, version)
 }
