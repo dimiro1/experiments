c138ac4e98041e832d6e3277ace6f048ea317963
claudemiro <dimiro1@gmail.com>
2016-05-02 06:39:59 -0300

    Passing variables to context in Gin

--- gin/main.go
+++ gin/main.go
@@ -2,17 +2,35 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type repository interface {
+	All() []string
+}
+
+type dummy struct{}
+
+func (d dummy) All() []string {
+	return []string{"Go", "Gin"}
+}
+
+func repositoryMiddleware(r repository) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.Set("repository", r)
+		ctx.Next()
+	}
+}
+
 func main() {
 	m := gin.Default()
+	m.Use(repositoryMiddleware(dummy{}))
 
 	m.GET("/", func(ctx *gin.Context) {
 		ctx.String(200, "Hello World")
 	})
 
 	m.GET("/json", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"hello": "world",
-		})
+		repo := ctx.MustGet("repository").(repository)
+
+		ctx.JSON(200, repo.All())
 	})
 
 	m.Run()
