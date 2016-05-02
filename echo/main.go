d88b180e8403aae820f943b42f3c8b4a00253d85
claudemiro <dimiro1@gmail.com>
2016-05-02 07:12:58 -0300

    Echo with custom context

--- echo/main.go
+++ echo/main.go
@@ -6,6 +6,23 @@ import (
 	"github.com/labstack/echo/middleware"
 )
 
+type myContext struct {
+	echo.Context
+}
+
+func (m myContext) HelloWorld() string {
+	return "Hello World"
+}
+
+func myContextMiddleware(m myContext) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(ctx echo.Context) error {
+			m.Context = ctx
+			return next(m)
+		}
+	}
+}
+
 type repository interface {
 	All() []string
 }
@@ -29,10 +46,16 @@ func main() {
 	m := echo.New()
 	m.Use(middleware.Logger())
 	m.Use(middleware.Recover())
+	m.Use(myContextMiddleware(myContext{}))
 	m.Use(repositoryMiddleware(dummy{}))
 
 	m.Get("/", func(ctx echo.Context) error {
 		return ctx.String(200, "Hello World")
+	})
+
+	m.Get("/custom", func(ctx echo.Context) error {
+		c := ctx.(myContext)
+		return ctx.String(200, c.HelloWorld())
 	})
 
 	m.Get("/json", func(ctx echo.Context) error {
