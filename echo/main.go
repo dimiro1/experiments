158ba4ce5366867e7224dc19f8ff2e2e14b755c7
claudemiro <dimiro1@gmail.com>
2016-05-02 06:40:09 -0300

    Passing variables to context in Echo

--- echo/main.go
+++ echo/main.go
@@ -6,19 +6,39 @@ import (
 	"github.com/labstack/echo/middleware"
 )
 
+type repository interface {
+	All() []string
+}
+
+type dummy struct{}
+
+func (d dummy) All() []string {
+	return []string{"Go", "Echo"}
+}
+
+func repositoryMiddleware(r repository) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(ctx echo.Context) error {
+			ctx.Set("repository", r)
+			return next(ctx)
+		}
+	}
+}
+
 func main() {
 	m := echo.New()
 	m.Use(middleware.Logger())
 	m.Use(middleware.Recover())
+	m.Use(repositoryMiddleware(dummy{}))
 
-	m.GET("/", func(ctx echo.Context) error {
+	m.Get("/", func(ctx echo.Context) error {
 		return ctx.String(200, "Hello World")
 	})
 
-	m.GET("/json", func(ctx echo.Context) error {
-		return ctx.JSON(200, map[string]string{
-			"hello": "world",
-		})
+	m.Get("/json", func(ctx echo.Context) error {
+		repo := ctx.Get("repository").(repository)
+
+		return ctx.JSON(200, repo.All())
 	})
 
 	m.Run(standard.New(":9090"))
